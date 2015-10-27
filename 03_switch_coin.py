beece5d96266918fc1e786a5c2119c6f3abf15b6
Simon Monk <srmonk@gmail.com>
2015-10-27 13:51:48 +0000

    bug fixed

--- 03_switch_coin.py
+++ 03_switch_coin.py
@@ -13,6 +13,7 @@ while True:
         rr.set_led1(0)
         rr.set_led2(1)
         time.sleep(0.015)
+    if rr.sw1_closed() == False:
         rr.set_led1(1)
         rr.set_led2(0)
         time.sleep(0.015)
