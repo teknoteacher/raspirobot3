392d2787cf559efeeec6b91adb992999f0a72f23
Simon Monk <Si@Simons-Mac-Pro.local>
2015-10-27 14:11:05 +0000

    added project 4 - ranger

---
+++ 04_movement.py
@@ -0,0 +1,28 @@
+# 04_movement.py
+# Uses the ultrasonic rangefinder to detect movement
+
+from rrb2 import *
+import time
+
+rr = RRB3()
+reference = rr.get_distance()
+rr.set_led1(0)
+rr.set_led2(0)
+
+print("alarm activated")
+print("Press CTRL-c to quit the program")
+
+while True:
+    time.sleep(0.3)
+    reading = rr.get_distance()
+    difference = reading - reference    
+    if difference < -1 or difference > 1:
+        print("Movement detected")
+        for a in range(5):        
+            rr.set_led1(1)
+            rr.set_led2(1)
+            time.sleep(0.3)
+            
+            rr.set_led1(0)
+            rr.set_led2(0)           
+            time.sleep(0.3)
